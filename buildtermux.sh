d36afa74921210f58e8c86d3c273db87186b09fe
Pascal <admin@serveurperso.com>
2026-03-13 17:13:14 +0100

    build: Android Termux -j 2 to avoid OOM during Vulkan shader compilation

--- buildtermux.sh
+++ buildtermux.sh
@@ -6,4 +6,4 @@ cd build
 
 cmake .. -DGGML_CPU_ALL_VARIANTS=ON -DGGML_VULKAN=ON -DGGML_BACKEND_DL=ON \
          -DVulkan_LIBRARY=/system/lib64/libvulkan.so
-cmake --build . --config Release -j "$(nproc)"
+cmake --build . --config Release -j 2
